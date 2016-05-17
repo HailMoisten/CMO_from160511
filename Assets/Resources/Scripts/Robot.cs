44e5475e0fb5b5ba8ed94293d792d87357375fff
HailMoisten <kirisame26@gmail.com>
2016-05-17 21:43:15 +0900

    Not recursion version.

--- Assets/Resources/Scripts/Robot.cs
+++ Assets/Resources/Scripts/Robot.cs
@@ -3,123 +3,123 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Robot : MonoBehaviour {
-	private TurnManager turnManager;
-	private List<int> CatchRouteID = new List<int>();
-	private Vector3 pos;// for calc.
+    private TurnManager turnManager;
+    private List<int> CatchOrderID = new List<int>();
+    private Vector3 pos;// for calc.
 
-	public void Awake()
-	{
-		turnManager = GameObject.Find("TurnManager").GetComponent<TurnManager>();
-	}
-	public void Initialize()
-	{
-		transform.position = Vector3.zero;
-		CatchRouteID.Clear();
-	}
+    public void Awake()
+    {
+        turnManager = GameObject.Find("TurnManager").GetComponent<TurnManager>();
+    }
+    public void Initialize()
+    {
+        transform.position = Vector3.zero;
+        CatchOrderID.Clear();
+    }
 
-	public float[] getNextMoveTime()
-	{
-		float[] ab = {2, 0};
-		return ab;
-	}
+    public float[] getNextMoveTime()
+    {
+        float[] ab = {2, 0};
+        return ab;
+    }
 
-	public void Calculate ()
-	{
-		CatchRouteID.Clear();
+    public void Calculate ()
+    {
+        CatchOrderID.Clear();
 
-	}
+    }
 
-	private void FullSearch()
-	{
-		Initialize();
-		int catchcount = 0;
-		float time = 0.0f;
-		pos = transform.position;
-		int catchresult = 0;
+    private void FullSearch()
+    {
+        Initialize();
+        int catchcount = 0;
+        float time = 0.0f;
+        pos = transform.position;
+        bool catchresult = true;
 
-		catchresult = CanCatch(pos.x, pos.y, 0, time);
+        for (int i = 0; i < TurnManager.BallList.Count; i++)
+        {
+            for (int d = 0; d < 5; d++)
+            {
+                if (CanCatch(pos, i, time, d, new List<int>()))
+                {
+                }
+            }
+        }
 
-		if (catchresult == 1)
-		{
-			catchcount++;
-			float nexttime = (Mathf.Abs(pos.x - TurnManager.BallList[0].nextPosition.x) / 2);
-			time = time + nexttime;
-			pos = TurnManager.BallList[0].nextPosition + (TurnManager.BallList[0].velocity * nexttime);
-		}
+    }
 
-		catchresult = CanCatch(pos.x, pos.y, 1, time);
+    private void SingleSearch()
+    {
+        for (int i = 0; i < TurnManager.BallList.Count; i++)
+        {
+            for (int d = 0; d < 5; d++)
+            {
+//                if (CanCatch(pos, i, time, d))
+            }
+        }
 
+    }
 
-		if (catchresult >= 2)
-		{
-		}
+    /// <summary>
+    /// Determines whether this instance can catch the specified pos ballnumber time dir.
+    /// dir:
+    /// 0: right, 1: rightchange, 2: change, 3: leftchange, 4: left.
+    /// </summary>
+    /// <returns><c>true</c> if this instance can catch the specified pos ballnumber time dir; otherwise, <c>false</c>.</returns>
+    /// <param name="pos">Position.</param>
+    /// <param name="ballnumber">Ballnumber.</param>
+    /// <param name="time">Time.</param>
+    /// <param name="dir">Dir.</param>
+    private bool CanCatch(Vector3 rpos, int ballnumber, float time, int dir, List<int> instlist)
+    {
+        Ball b = TurnManager.BallList[ballnumber];
+        Vector3 bpos = b.nextPosition + (time * b.velocity);
+        float usedtime = 0.0f;
 
-		if (catchresult >= 3)
-		{
-		}
+        switch (dir)
+        {
+            case 0:
+                if (rpos.y == bpos.y)
+                {
+                    if (rpos.x - bpos.x > 0 && b.velocity.x < 1)
+                    {
+                        usedtime = (Mathf.Abs(rpos.x - bpos.x) / 2);
+                        time = time + usedtime;// need
+                        bpos = bpos + (b.velocity * usedtime);
+                        rpos = bpos;// need
+                        return true;
+                    }
+                }
+                break;
+            case 1:
+                if (rpos.x - bpos.x < b.velocity.x * TurnManager.YRange * (Mathf.Sqrt(2) - 1))
+                {
+                }
+                break;
+            case 2:
+                if (rpos.x - bpos.x < b.velocity.x * TurnManager.YRange * 1)
+                {
+                }
+                break;
+            case 3:
+                if (rpos.x - bpos.x < b.velocity.x * TurnManager.YRange * (Mathf.Sqrt(2) + 1))
+                {
+                }
+                break;
+            case 4:
+                if (rpos.y == bpos.y)
+                {
+                    if (rpos.x - bpos.x < 0 && b.velocity.x > -1)
+                    {
+                        return true;
+                    }
+                }
+                break;
+            default: break;
+        }
 
-		if (catchresult >= 4)
-		{
-		}
+        return false;
+    }
 
-	}
-
-	/// <summary>
-	/// Determines whether this instance can catch the specified rx ry ballnumber.
-	/// 0: false, 1: only opposite, 2: only follow diagonal, 3: can virtical, 4: can against diagonal.
-	/// </summary>
-	/// <returns><c>true</c> if this instance can catch the specified rx ry ballnumber; otherwise, <c>false</c>.</returns>
-	/// <param name="rx">Rx.</param>
-	/// <param name="ry">Ry.</param>
-	/// <param name="ballnumber">Ballnumber.</param>
-	private int CanCatch(float rx, float ry, int ballnumber, float time)
-	{
-		Ball b = TurnManager.BallList[ballnumber];
-		Vector3 bpos = b.nextPosition + (time * b.velocity);
-		if (ry != b.nextPosition.y)
-		{
-			if (rx - bpos.x < b.velocity.x * TurnManager.YRange * (Mathf.Sqrt(2) - 1))
-			{
-				return 0;
-			}
-			else if (rx - bpos.x < b.velocity.x * TurnManager.YRange * 1)
-			{
-				return 2;
-			}
-			else if (rx - bpos.x < b.velocity.x * TurnManager.YRange * (Mathf.Sqrt(2) + 1))
-			{
-				return 3;
-			}
-			else
-			{
-				return 4;
-			}
-		}
-		else
-		{
-			if (b.velocity.x >= 1)
-			{
-				if (rx - bpos.x < 0)
-				{
-					return 0;
-				}
-				else
-				{
-					return 1;
-				}
-			}
-			else if (b.velocity.x <= -1)
-			{
-				if (rx - bpos.x > 0)
-				{
-					return 0;
-				}
-				else
-				{
-					return 1;
-				}
-			}
-		}
-		return 0;
-	}
 }
