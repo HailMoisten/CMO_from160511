faba2966fe7b745f9f1378b17e14590f95b5f75b
HailMoisten <kirisame26@gmail.com>
2016-05-18 15:51:42 +0900

    Little fix

--- Assets/Resources/Scripts/Robot.cs
+++ Assets/Resources/Scripts/Robot.cs
@@ -69,13 +69,8 @@ public class Robot : MonoBehaviour {
 
     private void FullSearch()
     {
-        List<List<float>> firstlist = new List<List<float>>();
-        firstlist.Add(new List<float>());
-        firstlist.Add(new List<float>());
-        firstlist.Add(new List<float>());
-        firstlist.Add(new List<float>());
-        firstlist.Add(new List<float>());
-
+        List<List<float>> resultlist = new List<List<float>>();
+     
         for (int i = 0; i < TurnManager.BallList.Count; i++)
         {
             for (int d = 0; d < 4; d++)
@@ -83,18 +78,29 @@ public class Robot : MonoBehaviour {
                 FullSearchByRecursion(transform.position, i, 0.0f, d, new List<List<float>>());
             }
         }
+/*
+        for (int i = 0; i < TurnManager.BallList.Count; i++)
+        {
+            for (int d = 0; d < 4; d++)
+            {
+                resultlist = FullSearchEnd(FullSearchPart(transform.position, i, 0.0f, d, new List<List<float>>()));
+            }
+        }
+*/
+
     }
     /// <summary>
-    /// Fulls the search by recursion.
+    /// Fulls the search part.
     /// dir:
     /// 0: horizontal, 1: rightchange, 2: virtical, 3: leftchange.
     /// </summary>
+    /// <returns>The search part.</returns>
     /// <param name="rpos">Rpos.</param>
-    /// <param name="ballnumber">Ballnumber.</param>
+    /// <param name="ballid">Ballid.</param>
     /// <param name="time">Time.</param>
     /// <param name="dir">Dir.</param>
-    /// <param name="instlist">Instlist.</param>
-    private void FullSearchByRecursion(Vector3 rpos, int ballid, float time, int dir, List<List<float>> calclist)
+    /// <param name="calclist">Calclist.</param>
+    private List<List<float>> FullSearchPart(Vector3 rpos, int ballid, float time, int dir, List<List<float>> calclist)
     {
         if (calclist.Count == 0)
         {
@@ -108,7 +114,7 @@ public class Robot : MonoBehaviour {
         if (calclist[0].Contains(ballid))
         {
             // end of recursion.
-            FullSearchEnd(calclist);
+            return FullSearchEnd(calclist);
         }
         else
         {
@@ -231,6 +237,192 @@ public class Robot : MonoBehaviour {
                 calclist[3].Add(rpos.x);
                 calclist[4].Add(rpos.y);
 
+                return calclist;
+
+            }
+            else
+            {
+                // end of recursion.
+                return FullSearchEnd(calclist);
+            }
+        }
+
+    }
+
+    private List<List<float>> FullSearchEnd(List<List<float>> calclist)
+    {
+        if (CatchOrder[0].Count < calclist[0].Count)
+        {
+            CatchOrder.Clear();
+            CatchOrder.Add(calclist[0]);
+            CatchOrder.Add(calclist[1]);
+            CatchOrder.Add(calclist[2]);
+            CatchOrder.Add(calclist[3]);
+            CatchOrder.Add(calclist[4]);
+            Debug.Log("time: ");
+            for (int i = 0; i < CatchOrder[2].Count; i++)
+            {
+                Debug.Log(CatchOrder[2][i]);
+            }
+        }
+        return CatchOrder;
+    }
+
+    /// <summary>
+    /// Fulls the search by recursion.
+    /// dir:
+    /// 0: horizontal, 1: rightchange, 2: virtical, 3: leftchange.
+    /// </summary>
+    /// <param name="rpos">Rpos.</param>
+    /// <param name="ballnumber">Ballnumber.</param>
+    /// <param name="time">Time.</param>
+    /// <param name="dir">Dir.</param>
+    /// <param name="instlist">Instlist.</param>
+    private void FullSearchByRecursion(Vector3 rpos, int ballid, float time, int dir, List<List<float>> delivlist)
+    {
+        if (delivlist.Count == 0)
+        {
+            delivlist.Add(new List<float>());
+            delivlist.Add(new List<float>());
+            delivlist.Add(new List<float>());
+            delivlist.Add(new List<float>());
+            delivlist.Add(new List<float>());
+        }
+
+        if (delivlist[0].Contains(ballid))
+        {
+            // end of recursion.
+            FullSearchEndByRecursion(delivlist);
+        }
+        else
+        {
+            Ball b = TurnManager.BallList[ballid];
+            Vector3 bpos = b.nextPosition + (time * b.velocity);
+            float usedtime = 0.0f;
+            bool cancatch = false;
+
+            switch (dir)
+            {
+                case 0:
+                    if (rpos.y == bpos.y)
+                    {
+                        if (b.velocity.x > 0 && (rpos.x - bpos.x) >= 0)
+                        {
+                            cancatch = true;
+                        }
+                        else if (b.velocity.x < 0 && (rpos.x - bpos.x) <= 0)
+                        {
+                            cancatch = true;
+                        }
+                    }
+                    break;
+                case 1:
+                    if (rpos.y != bpos.y)
+                    {
+                        if (b.velocity.x > 0 && rpos.x - bpos.x >= TurnManager.YRange * (Mathf.Sqrt(2) - 1))
+                        {
+                            cancatch = true;
+                        }
+                        else if (b.velocity.x < 0 && rpos.x - bpos.x <= -1 * TurnManager.YRange * (Mathf.Sqrt(2) + 1))
+                        {
+                            cancatch = true;
+                        }
+                    }
+                    break;
+                case 2:
+                    if (rpos.y != bpos.y)
+                    {
+                        if (b.velocity.x > 0 && (rpos.x - bpos.x) >= TurnManager.YRange * 1)
+                        {
+                            cancatch = true;
+                        }
+                        else if (b.velocity.x < 0 && (rpos.x - bpos.x) <= TurnManager.YRange * 1)
+                        {
+                            cancatch = true;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (rpos.y != bpos.y)
+                    {
+                        if (b.velocity.x < 0 && bpos.x - rpos.x >= TurnManager.YRange * (Mathf.Sqrt(2) - 1))
+                        {
+                            cancatch = true;
+                        }
+                        else if (b.velocity.x > 0 && rpos.x - bpos.x >= TurnManager.YRange * (Mathf.Sqrt(2) + 1))
+                        {
+                            cancatch = true;
+                        }
+                    }
+                    break;
+                default :
+                    break;
+            }
+
+            if (cancatch)
+            {
+                switch (dir)
+                {
+                    case 0: 
+                        break;
+                    case 1:
+                        usedtime = TurnManager.YRange * (Mathf.Sqrt(2));
+                        bpos = bpos + (usedtime * b.velocity);
+                        if (bpos.y > rpos.y)
+                        {
+                            rpos = rpos + ((Vector3.right + Vector3.up) * TurnManager.YRange);
+                        }
+                        else
+                        {
+                            rpos = rpos + ((Vector3.right + Vector3.down) * TurnManager.YRange);
+                        }
+                        break;
+                    case 2:
+                        usedtime = TurnManager.YRange;
+                        bpos = bpos + (usedtime * b.velocity);
+                        if (bpos.y > rpos.y)
+                        {
+                            rpos = rpos + (Vector3.up * TurnManager.YRange);
+                        }
+                        else
+                        {
+                            rpos = rpos + (Vector3.down * TurnManager.YRange);
+                        }
+                        break;
+                    case 3:
+                        usedtime = TurnManager.YRange * (Mathf.Sqrt(2));
+                        bpos = bpos + (usedtime * b.velocity);
+                        if (bpos.y > rpos.y)
+                        {
+                            rpos = rpos + ((Vector3.left + Vector3.up) * TurnManager.YRange);
+                        }
+                        else
+                        {
+                            rpos = rpos + ((Vector3.left + Vector3.down) * TurnManager.YRange);
+                        }
+                        break;
+                    default :
+                        break;
+                }
+                usedtime = usedtime + (Mathf.Abs(rpos.x - bpos.x) / 2);
+                time = time + usedtime;// need
+                bpos = bpos + (b.velocity * (Mathf.Abs(rpos.x - bpos.x) / 2));
+                rpos = bpos;// need
+
+
+                List<List<float>> calclist = new List<List<float>>();
+                calclist.Add(delivlist[0]);
+                calclist.Add(delivlist[1]);
+                calclist.Add(delivlist[2]);
+                calclist.Add(delivlist[3]);
+                calclist.Add(delivlist[4]);
+
+                calclist[0].Add(b.identification);
+                calclist[1].Add(dir);
+                calclist[2].Add(time);
+                calclist[3].Add(rpos.x);
+                calclist[4].Add(rpos.y);
+
                 // Recursion
                 for (int i = 0; i < TurnManager.BallList.Count; i++)
                 {
@@ -244,22 +436,22 @@ public class Robot : MonoBehaviour {
             else
             {
                 // end of recursion.
-                FullSearchEnd(calclist);
+                FullSearchEndByRecursion(delivlist);
             }
         }
 
     }
 
-    private void FullSearchEnd(List<List<float>> calclist)
+    private void FullSearchEndByRecursion(List<List<float>> delivlist)
     {
-        if (CatchOrder[0].Count < calclist[0].Count)
+        if (CatchOrder[0].Count < delivlist[0].Count)
         {
             CatchOrder.Clear();
-            CatchOrder.Add(calclist[0]);
-            CatchOrder.Add(calclist[1]);
-            CatchOrder.Add(calclist[2]);
-            CatchOrder.Add(calclist[3]);
-            CatchOrder.Add(calclist[4]);
+            CatchOrder.Add(delivlist[0]);
+            CatchOrder.Add(delivlist[1]);
+            CatchOrder.Add(delivlist[2]);
+            CatchOrder.Add(delivlist[3]);
+            CatchOrder.Add(delivlist[4]);
             Debug.Log("time: ");
             for (int i = 0; i < CatchOrder[2].Count; i++)
             {
